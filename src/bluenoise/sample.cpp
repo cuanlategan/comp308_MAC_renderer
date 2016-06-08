622954d755cc4eaa22b3da84ff43db756ffa755e
Adrian Cochrane <alcinnz@Adrians-Mini.station>
2016-06-08 12:53:46 +1200

    Start implementing bluenoise sampling
    This implements the geometric aspects.

---
+++ src/bluenoise/sample.cpp
@@ -0,0 +1,9 @@
+//
+//  sample.cpp
+//  
+//
+//  Created by Adrian Cochrane on 8/06/16.
+//
+//
+
+#include "sample.hpp"
---
+++ src/bluenoise/sample.hpp
@@ -0,0 +1,14 @@
+//
+//  sample.hpp
+//  
+//
+//  Created by Adrian Cochrane on 8/06/16.
+//
+//
+
+#ifndef sample_hpp
+#define sample_hpp
+
+#include <stdio.h>
+
+#endif /* sample_hpp */
